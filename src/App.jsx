ae6434a8ff284fb04f601daf7ab2ec72bfd81fbc
Syed Ayaan Ali <94982193+incognito-root@users.noreply.github.com>
2025-11-18 19:29:50 +0500

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { supabase } from './lib/supabase';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
